a7683cb4ddb73cf4199adfdd91606c517723006d
Chris Dargis <chris.dargis@steelseries.com>
2013-11-18 00:25:44 -0600

    updating sql shell script

--- backend/src/generateSQL.sh
+++ backend/src/generateSQL.sh
@@ -5,29 +5,40 @@ USERSCSV=users.csv
 OUTFILE="seed.sql"
 
 function create_users_script () {
-    local file=$OUTFILE
+
+    echo "-- Users" >> "$OUTFILE"
+    local count=0
 
     OLDIFS=$IFS
     IFS=,
     while read PUBLICID NAME EMAIL
     do
-        echo "INSERT INTO Users (publicId, name, email) VALUES ('$PUBLICID','$NAME','$EMAIL');" >> "$file"
+        if [ $count -ne 0 ]; then
+            echo "INSERT INTO Users (publicId, name, email) VALUES ('$PUBLICID','$NAME','$EMAIL');" >> "$OUTFILE"
+        fi
+        let "count += 1"
     done < $USERSCSV
     IFS=$OLDIFS
 }
 
 function create_flags_script () {
-    local file=$OUTFILE
+
+    echo "-- Flags" >> "$OUTFILE"
+    local count=0
 
     OLDIFS=$IFS
     IFS=,
     while read HOST HASH VALUE COMMENTS
     do
-        echo "INSERT INTO Flags (host, tag, value, comment) VALUES ('$HOST', '$HASH', '$VALUE', '$COMMENTS');" >> "$file"
+        if [ $count -ne 0 ]; then
+            echo "INSERT INTO Flags (host, tag, value, comment) VALUES ('$HOST', '$HASH', '$VALUE', '$COMMENTS');" >> "$OUTFILE"
+        fi
+        let "count += 1"
     done < $FLAGCSV
     IFS=$OLDIFS
 }
 
 > $OUTFILE
 create_users_script
+echo "" >> "$OUTFILE"
 create_flags_script
